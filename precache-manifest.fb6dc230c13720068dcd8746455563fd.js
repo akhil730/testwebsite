1f3718d9fa639127c62ce9623ef2d9fec97d5f77
akhil <akhilg919@gmail.com>
2022-05-22 14:19:32 +0530

    Updates

---
+++ precache-manifest.fb6dc230c13720068dcd8746455563fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2889b58c0fa98bb85679",
+    "url": "/testwebsite/static/css/main.6049c9c6.chunk.css"
+  },
+  {
+    "revision": "2889b58c0fa98bb85679",
+    "url": "/testwebsite/static/js/main.2889b58c.chunk.js"
+  },
+  {
+    "revision": "4fd4b15f5d9ae667ee48",
+    "url": "/testwebsite/static/js/1.4fd4b15f.chunk.js"
+  },
+  {
+    "revision": "c2279e8b570555bc3d63",
+    "url": "/testwebsite/static/js/runtime~main.c2279e8b.js"
+  },
+  {
+    "revision": "3272c15c579cfa75e630256ad3a8d0b6",
+    "url": "/testwebsite/static/media/photoo.3272c15c.png"
+  },
+  {
+    "revision": "2b42d44b434f5d565f7566d7378ad3fb",
+    "url": "/testwebsite/static/media/video.2b42d44b.bin"
+  },
+  {
+    "revision": "dfba59753abaef0134f4c8c103355a87",
+    "url": "/testwebsite/index.html"
+  }
+];
